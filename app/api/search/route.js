609c281ff7fbc27cf557c9f2608297ed3e982d4b
Sachin <sachinsunny303@gmail.com>
2024-04-18 06:07:49 +0100

    ADD prompt search for the page

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -5,35 +5,30 @@ import UserModel from '@models/user';
 export const POST = async (request) => {
   const {searchText} = await request.json();
 
-  console.log(searchText);
+  try {
+    await connectToDB();
 
-  if (searchText.includes('#')) {
-    try {
-      await connectToDB();
-      const userPosts = await PromptModel.find({
-        tag: {$regex: searchText, $options: 'i'},
-      }).populate('creator');
-      return new Response(JSON.stringify(userPosts));
-    } catch (error) {
-      console.log('search error', error);
-      return new Response(error);
-    }
-  } else {
-    try {
-      await connectToDB();
-      const userData = await UserModel.find({
-        username: {$regex: searchText, $options: 'i'},
-      });
+    const userQuery = {
+      username: {$regex: searchText, $options: 'i'},
+    };
 
-      const userIds = userData.map((user) => user._id);
-      const userPosts = await PromptModel.find({
-        creator: {$in: userIds},
-      }).populate('creator');
+    const promptQuery = {
+      $or: [
+        {tag: {$regex: searchText, $options: 'i'}},
+        {prompt: {$regex: searchText, $options: 'i'}},
+      ],
+    };
 
-      return new Response(JSON.stringify(userPosts));
-    } catch (error) {
-      console.log('search error', error);
-      return new Response(error);
-    }
+    const userData = await UserModel.find(userQuery);
+    const userIds = userData.map((user) => user._id);
+
+    const userPosts = await PromptModel.find({
+      $or: [{creator: {$in: userIds}}, promptQuery],
+    }).populate('creator');
+
+    return new Response(JSON.stringify(userPosts));
+  } catch (error) {
+    console.log('search error', error);
+    return new Response(error);
   }
 };
